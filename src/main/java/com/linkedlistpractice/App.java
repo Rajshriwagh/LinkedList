b3e0082644cbe2c37ea4ecb3406071f748851aca
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-10-02 22:51:06 +0530

    ADD: Added function to sort linked list using bubble sort

--- src/main/java/com/linkedlistpractice/App.java
+++ src/main/java/com/linkedlistpractice/App.java
@@ -6,20 +6,24 @@ import org.apache.logging.log4j.Logger;
 public class App {
 	private static final Logger logger = LogManager.getLogger(App.class);
 
-    public static void main(String[] args) {
-    	logger.info("Hello to Linked List Practise Problem!");
-        Linkedlist list = new Linkedlist();
-        // list.insertNode(56);
-        // list.insertNode(30);
-        // list.insertNode(70);
-        list.insertAtStart(70);
-        // list.insertNode(30);
-        list.insertAtStart(56);
-        list.insertAtIndex(1, 30);
-        // list.deleteNode(0);
-        list.deleteNode(2);
-        list.showLinkedList();
-        list.searchNodeWithKey(30);
-        list.sizeOfLinkedList();
-    }
+	public static void main(String[] args) {
+		logger.info("Hello to Linked List Practise Problem!");
+		Linkedlist list = new Linkedlist();
+		// list.insertNode(56);
+		// list.insertNode(30);
+		// list.insertNode(70);
+		list.insertAtStart(70);
+		// list.insertNode(30);
+		list.insertAtStart(56);
+		list.insertAtIndex(1, 30);
+		list.insertAtIndex(2, 40);
+		// list.deleteNode(0);
+		// list.deleteNode(2);
+		list.showLinkedList();
+		list.searchNodeWithKey(30);
+		list.sizeOfLinkedList();
+		list.orderedLinkedList();
+		list.showLinkedList();
+
+	}
 }
