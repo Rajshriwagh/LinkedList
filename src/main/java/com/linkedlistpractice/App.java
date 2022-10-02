c3a00a1b1f81634af1f68f2f140bd458c864fc51
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-10-02 21:58:22 +0530

    ADD: Added LinkedList Implementaion and inserted 3 nodes and displayed them

--- src/main/java/com/linkedlistpractice/App.java
+++ src/main/java/com/linkedlistpractice/App.java
@@ -9,18 +9,10 @@ public class App {
 	public static void main(String[] args) {
 		logger.info("Hello to Linked List Practise Problem!");
 		Linkedlist list = new Linkedlist();
-		// list.insertNode(56);
-		// list.insertNode(30);
-		// list.insertNode(70);
-		list.insertAtStart(70);
-		// list.insertNode(30);
-		list.insertAtStart(56);
-		list.insertAtIndex(1, 30);
-		list.insertAtIndex(2, 40);
-		// list.deleteNode(0);
-		// list.deleteNode(2);
+		list.insertNode(56);
+		list.insertNode(30);
+		list.insertNode(70);
 		list.showLinkedList();
-		list.searchNodeWithKey(30);
-		list.sizeOfLinkedList();
+
 	}
 }
--- src/main/java/com/linkedlistpractice/Linkedlist.java
+++ src/main/java/com/linkedlistpractice/Linkedlist.java
@@ -1,24 +1,7 @@
 package com.linkedlistpractice;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Linkedlist {
-	private static final Logger logger = LogManager.getLogger(App.class);
-    Node head;
-
-    void insertAtStart(int data) {
-        Node node = new Node();
-        node.data = data;
-        node.next = null;
-        if (head == null) {
-            head = node;
-        } else {
-            Node temp;
-            temp = head;
-            head = node;
-            head.next = temp;
-        }
-    }
+	Node head;
 
     void insertNode(int data) {
         Node node = new Node();
@@ -36,123 +19,14 @@ public class Linkedlist {
         }
     }
 
-    void insertAtIndex(int index, int data) {
-        Node node = new Node();
-        node.data = data;
-        node.next = null;
-        if (head == null) {
-            insertAtStart(data);
-        } else {
-            Node temp;
-            temp = head;
-            for (int i = 0; i < index - 1; i++) {
-                temp = temp.next;
-            }
-            node.next = temp.next;
-            temp.next = node;
-        }
-    }
-
     void showLinkedList() {
         Node temp = head;
         while (temp.next != null) {
-            logger.info(temp.data);
+            System.out.println(temp.data);
             temp = temp.next;
         }
-        logger.info(temp.data);
-    }
-
-    void deleteNode(int index) {
-        Node node;
-        Node temp;
-        temp = head;
-        if (head == null) {
-            logger.info("NO list exists!!");
-        } else {
-
-            for (int i = 0; i < index - 1; i++) {
-                temp = temp.next;
-            }
-            if (index == 0) {
-
-                head = head.next;
-                temp = null;
-
-            } else {
-                node = temp.next;
-                temp.next = node.next;
-                node = null;
-            }
-
-        }
+        System.out.println(temp.data);
 
     }
 
-    void searchNodeWithKey(int data) {
-        Node node = new Node();
-        node.data = data;
-        node.next = null;
-        if (head == null) {
-            logger.info("Linked list empty!!");
-        } else {
-            Node temp;
-            temp = head;
-            while (temp != null) {
-                if (temp.data == node.data) {
-                    logger.info("Node Found " + node.data);
-                    break;
-                } else {
-                    temp = temp.next;
-                }
-            }
-
-        }
-    }
-
-    void sizeOfLinkedList() {
-        Node temp;
-        temp = head;
-        int count = 0;
-        if (head == null) {
-            logger.info("List is empty!");
-        } else {
-            while (temp.next != null) {
-                temp = temp.next;
-                count++;
-            }
-            count++;
-            logger.info("Size of Linked list is " + count);
-        }
-
-    }
-
-    void orderedLinkedList() {
-        Node current = head, index = null;
-
-        int temp;
-
-        if (head == null) {
-            return;
-        } else {
-            while (current != null) {
-                // Node index will point to node next to
-                // current
-                index = current.next;
-
-                while (index != null) {
-                    // If current node's data is greater
-                    // than index's node data, swap the data
-                    // between them
-                    if (current.data > index.data) {
-                        temp = current.data;
-                        current.data = index.data;
-                        index.data = temp;
-                    }
-
-                    index = index.next;
-                }
-                current = current.next;
-            }
-        }
-    }
 }
